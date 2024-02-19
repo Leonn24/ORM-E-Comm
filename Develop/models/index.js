517c7864f65da2ba0a90e4e2802289a40ae4c4b9
Leonn24 <82012128+Leonn24@users.noreply.github.com>
2024-02-18 20:04:38 -0500

    imported starter code and added fields and rules to models

---
+++ Develop/models/index.js
@@ -0,0 +1,31 @@
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+
+Product.belongsTo(Category, {
+  foreginKey: 'catagory_id'
+});
+
+Product.belongsToMany(tag, {
+  through: ProductTag,
+  foreginKey: 'product_id',
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreginKey: 'tag_id',
+});
+
+Category.hasMany(Product, {
+  foreginKey: 'catagory_id'
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
